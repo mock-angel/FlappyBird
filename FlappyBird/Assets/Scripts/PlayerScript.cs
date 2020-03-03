fb0bda2205e68fb00fcd3dd4d28da25b0db22daa
Anantha Krishna R <sujakrishnar@gmail.com>
2020-03-03 21:22:37 +0530

    Created Working level

--- FlappyBird/Assets/Scripts/PlayerScript.cs
+++ FlappyBird/Assets/Scripts/PlayerScript.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public GameObject gameOverScreen;
-    
+    public GameObject gameWonScreen;
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D collision){
-        print("runnign here");
         if(collision.gameObject.tag == "walls")
         {
             gameOverScreen.SetActive(true);
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "finish")
+        {
+            gameWonScreen.SetActive(true);
             Destroy(gameObject);
         }
     }
